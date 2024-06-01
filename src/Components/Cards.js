7bb015cbe5b431f8f53f522e43483946b71bca01
Aditi Chaurasia <103958459+aditi-chaurasia@users.noreply.github.com>
2024-06-01 10:52:48 +0530

    Cards.js

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,53 +1,53 @@
-// import Card from 'react-bootstrap/Card';
-// import './App.css'
-// import { useEffect, useState} from 'react';
-// import { Link } from "react-router-dom";
-// import { useParams } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import './App.css'
+import { useEffect, useState} from 'react';
+import { Link } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
-// function Cards() {
+function Cards() {
     
-//    const[movies,setmovies]=useState([])
-//    const {type}=useParams()
-//     const fetchData=async()=>{
-//       let response= await fetch(`https://api.themoviedb.org/3/movie/${type ? type:"popular"}?api_key=e2abee6c8ed2414f5b23970f331d7cd8`)
-//       let data=await response.json();
-//         setmovies(data.results)
-//     }
-//     console.log(movies)
-//     useEffect(()=>{
-//         fetchData();
-//     },[])
-//     useEffect(()=>{
-//       fetchData();
-//   },[type])
-//   return (
-//     <>
+   const[movies,setmovies]=useState([])
+   const {type}=useParams()
+    const fetchData=async()=>{
+      let response= await fetch(`https://api.themoviedb.org/3/movie/${type ? type:"popular"}?api_key=e2abee6c8ed2414f5b23970f331d7cd8`)
+      let data=await response.json();
+        setmovies(data.results)
+    }
+    console.log(movies)
+    useEffect(()=>{
+        fetchData();
+    },[])
+    useEffect(()=>{
+      fetchData();
+  },[type])
+  return (
+    <>
 
-//     <div className='main'>
-//        <div className='main2'>
+    <div className='main'>
+       <div className='main2'>
 
-//         {movies.map((ele) =>{return(
-//             <>
-//             <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${ele.id}`} >
+        {movies.map((ele) =>{return(
+            <>
+            <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${ele.id}`} >
            
-//             < Card className='card' border="danger">
+            < Card className='card' border="danger">
         
-//         <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${ele.poster_path}`} height={'240rem'}/>
-//         <Card.Body>
-//        <Card.Title className='card-title'>{ele.title}</Card.Title>
+        <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${ele.poster_path}`} height={'240rem'}/>
+        <Card.Body>
+       <Card.Title className='card-title'>{ele.title}</Card.Title>
        
-//         </Card.Body>
-//       </Card>
-//       </Link>
-//             </>
-//         )
-//         })
-//     }
-//     </div>
-//     </div>
+        </Card.Body>
+      </Card>
+      </Link>
+            </>
+        )
+        })
+    }
+    </div>
+    </div>
    
-//     </>
-//   ); 
-// }
+    </>
+  ); 
+}
 
-// export default Cards;
+export default Cards;
